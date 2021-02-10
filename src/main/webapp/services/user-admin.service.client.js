a625b2761d7f6d53a6f4d701ffa4c06028eddb32
yonnie <yonathanmichael63@gmail.com>
2021-02-10 15:39:20 -0500

    connected to back end. Full Functionality installed.

--- src/main/webapp/services/user-admin.service.client.js
+++ src/main/webapp/services/user-admin.service.client.js
@@ -6,19 +6,41 @@ function AdminUserServiceClient() {
     this.updateUser = updateUser;
     this.url = 'https://wbdv-generic-server.herokuapp.com/api/001069339/users';
     var self = this;
+
     function createUser(user) {
-      return fetch(self.url, {
+        return fetch(self.url, {
         method: 'POST',
         headers: {
             'content-type': 'application/json'
         },
         body: JSON.stringify(user)
-        }).then(function (response) {
+        }).then(function(response) {
             return response.json()
         })
     }
-    function findAllUsers() {  }
-    function findUserById(userId) {  }
-    function updateUser(userId, user) {  }
-    function deleteUser(userId) {  }
+
+    function findAllUsers() {
+      return fetch(self.url)
+        .then(function (response) {
+            return response.json()
+      })
+    }
+
+    function findUserById(userId) {
+    }
+
+    function updateUser(userId, user) {
+       return fetch(`${self.url}/${userId}`, {
+         method: 'PUT',
+         headers: {
+            'content-type': 'application/json'
+         },
+         body: JSON.stringify(user)
+         }).then(response => response.json())
+    }
+
+    function deleteUser(userId) {
+        return fetch(`${self.url}/${userId}`,
+         {method: 'DELETE'})
+    }
 }
